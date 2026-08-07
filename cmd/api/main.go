ceb337bb4730c7900f07c1c88a49b70d93ae319d
Denis Araujo <denisgabriel017@gmail.com>
2026-08-07 19:10:50 -0300

    feat: add finalização do server e log no terminal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/DenisGabriel017/fiscalsync-go/internal/platform/config"
 	"github.com/joho/godotenv"
@@ -25,6 +30,39 @@ func main() {
 		w.Write([]byte(`{"status":"ok"}`))
 	})
 
-	log.Fatal(http.ListenAndServe(":"+cfg.HTTPPort, mux))
+	server := &http.Server{
+		Addr:              ":" + cfg.HTTPPort,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
 
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	log.Println("encerrando servidor")
+
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+
+	defer cancel()
+
+	err := server.Shutdown(shutdownCtx)
+
+	if err != nil {
+		log.Printf("erro ao encerrar servidor: %v", err)
+		return
+	}
+
+	log.Println("servidor encerrado")
 }
